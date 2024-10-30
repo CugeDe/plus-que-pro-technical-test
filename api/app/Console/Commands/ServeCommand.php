26f1cad4bb40552ed02ae3033adf068caa3cb831
CugeDe <cugede.0x07ffff@gmail.com>
2024-10-30 19:02:47 +0100

    [+] Overrides ServeCommand to let the correct env. vars passthrough

---
+++ api/app/Console/Commands/ServeCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Foundation\Console\ServeCommand as ServeCommandBase;
+
+class ServeCommand extends ServeCommandBase
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'serve';
+
+    /**
+     * The environment variables that should be passed from host machine to the PHP server process.
+     *
+     * @var string[]
+     */
+    public static $passthroughVariables = [
+        'APP_ENV',
+        'DB_CONNECTION',
+        'DB_DATABASE',
+        'DB_HOST',
+        'DB_USERNAME',
+        'DB_PASSWORD',
+        'HERD_PHP_81_INI_SCAN_DIR',
+        'HERD_PHP_82_INI_SCAN_DIR',
+        'HERD_PHP_83_INI_SCAN_DIR',
+        'IGNITION_LOCAL_SITES_PATH',
+        'LARAVEL_SAIL',
+        'PATH',
+        'PHP_CLI_SERVER_WORKERS',
+        'PHP_IDE_CONFIG',
+        'SYSTEMROOT',
+        'XDEBUG_CONFIG',
+        'XDEBUG_MODE',
+        'XDEBUG_SESSION',
+    ];
+}
